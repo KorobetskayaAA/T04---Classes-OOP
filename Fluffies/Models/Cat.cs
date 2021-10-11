61e2fabc16bd8f530fb40d9386786287fc133651
A. Korobetskaya <kornast@yandex.ru>
2021-10-11 10:00:40 +0400

    Fluffies v0.0.3 - Base class Pet

--- Fluffies/Models/Cat.cs
+++ Fluffies/Models/Cat.cs
@@ -4,61 +4,9 @@ using System.Text;
 
 namespace Fluffies
 {
-    class Cat
+    class Cat : Pet
     {
-        public string Name { get; set; }
-        public int Age { get; private set; } = 0;
-        double hunger = 5;
-        public double Hunger 
-        { 
-            get { return hunger; } 
-            set {
-                if (value < 0)
-                    hunger = 0;
-                else if (value > 10)
-                    hunger = 10;
-                else hunger = value;
-            } 
-        }
-        double tiredness = 5;
-        public double Tiredness
-        {
-            get { return tiredness; }
-            set
-            {
-                if (value < 0)
-                    tiredness = 0;
-                else if (value > 10)
-                    tiredness = 10;
-                else tiredness = value;
-            }
-        }
-        double happiness = 5;
-        public double Happiness
-        {
-            get { return happiness; }
-            set
-            {
-                if (value < 0)
-                    happiness = 0;
-                else if (value > 10)
-                    happiness = 10;
-                else happiness = value;
-            }
-        }
-
-        public Cat(string name)
-        {
-            Name = name;
-        }
-
-        public bool IsTired => Tiredness > 5;
-        public bool IsVeryTired => Tiredness > 9;
-        public bool IsHungry => Hunger > 6;
-        public bool IsVeryHungry => Hunger > 8;
-        public bool IsFull => Hunger < 2;
-        public bool IsAngry => Happiness < 3;
-        public bool IsHappy => Happiness > 8;
+        public Cat(string name) : base(name) { }
 
         public void DoNothing()
         {
@@ -138,9 +86,9 @@ namespace Fluffies
             }
         }
 
-        public void UpdateState()
+        public override void UpdateState()
         {
-            Age++;
+            base.UpdateState();
             if (IsFull)
             {
                 Happiness += 1;
---
+++ Fluffies/Models/Pet.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fluffies
+{
+    class Pet
+    {
+        public Pet(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; private set; } = 0;
+        double hunger = 5;
+        public double Hunger
+        {
+            get { return hunger; }
+            set
+            {
+                if (value < 0)
+                    hunger = 0;
+                else if (value > 10)
+                    hunger = 10;
+                else hunger = value;
+            }
+        }
+        double tiredness = 5;
+        public double Tiredness
+        {
+            get { return tiredness; }
+            set
+            {
+                if (value < 0)
+                    tiredness = 0;
+                else if (value > 10)
+                    tiredness = 10;
+                else tiredness = value;
+            }
+        }
+        double happiness = 5;
+        public double Happiness
+        {
+            get { return happiness; }
+            set
+            {
+                if (value < 0)
+                    happiness = 0;
+                else if (value > 10)
+                    happiness = 10;
+                else happiness = value;
+            }
+        }
+
+        public bool IsTired => Tiredness > 5;
+        public bool IsVeryTired => Tiredness > 9;
+        public bool IsHungry => Hunger > 6;
+        public bool IsVeryHungry => Hunger > 8;
+        public bool IsFull => Hunger < 2;
+        public bool IsAngry => Happiness < 3;
+        public bool IsHappy => Happiness > 8;
+
+        public virtual void UpdateState()
+        {
+            Age++;
+        }
+    }
+}
